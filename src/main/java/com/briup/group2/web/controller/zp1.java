bad8446ee35084d082b6ed73fd03cfbb47b092aa
zhangpan <1295762586@qq.com>
2019-12-24 19:41:06 +0800

    我的文件

---
+++ src/main/java/com/briup/group2/web/controller/zp1.java
@@ -0,0 +1,22 @@
+package com.briup.group2.web.controller;
+
+public class zp1 {
+    private String name;
+    private int sno;
+
+    public int getSno() {
+        return sno;
+    }
+
+    public void setSno(int sno) {
+        this.sno = sno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
